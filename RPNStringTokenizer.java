d9926ee93554431a4348c23f82996fa05a43e577
vbono <80070331+vbono@users.noreply.github.com>
2023-02-27 10:43:54 -0500

    update comments

--- RPNStringTokenizer.java
+++ RPNStringTokenizer.java
@@ -8,14 +8,17 @@ public interface RPNStringTokenizer {
 		// take a string.  If it is valid RPN stuff - integers or operators
 		// - then put them in a list for processing.
 		// anything bad (not int or one of the operators we like) and return a null.
-		// YOU WRITE THIS!
 		String[] spl = expression.split(" ");
 		
+		// load new array list with accepted operators
 		String ops = "+,-,*,/,^";
 		String[] valid = ops.split(",");
 		ArrayList<String> operators = new ArrayList<>(Arrays.asList(valid));
+		
+		// create new array 
 		ArrayList<String> equation = new ArrayList<>();
-
+		
+		// formats input String expression to ArrayList equation, also tests validity of characters in string
 		for (int i = 0; i < spl.length; i++) {
 				if (operators.contains(spl[i])) {
 					equation.add(spl[i]);
